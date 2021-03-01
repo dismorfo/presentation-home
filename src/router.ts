3c0c5a2d6a04d41a812cd9063320dbede63af2a6
Alberto Ortiz Flores <aortiz@nyu.edu>
2021-03-01 17:12:36 -0500

    netlify

--- src/router.ts
+++ src/router.ts
@@ -13,25 +13,6 @@ export default new Router({
       component: () => import(/* webpackChunkName: "home" */ './views/Home.vue'),
     },
     {
-      path: '/embed/:resource/:identifier/:sequence',
-      name: 'embedded',
-      component: () => import(/* webpackChunkName: "embedded" */ './views/Embedded.vue'),
-      props: route => ({
-        resource: route.params.resource,
-        identifier: route.params.identifier,
-        sequence: Number(route.params.sequence),
-      }),
-    },
-    {
-      path: '/mirador/:resource/:identifier',
-      name: 'mirador',
-      component: () => import(/* webpackChunkName: "mirador" */ './views/Mirador.vue'),
-      props: route => ({
-        resource: route.params.resource,
-        identifier: route.params.identifier,
-      }),
-    },
-    {
       path: '/:resource/:identifier/:sequence',
       name: 'viewer',
       component: () => import(/* webpackChunkName: "viewer" */ './views/Viewer.vue'),
